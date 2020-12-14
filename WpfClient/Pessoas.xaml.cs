b86d59ddcbd263f595e0499330c12b9b93a6a449
Jeferson <jefersonblc@gmail.com>
2020-12-14 12:48:38 -0300

    Atualizado Api Client

--- WpfClient/Pessoas.xaml.cs
+++ WpfClient/Pessoas.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Rest;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -9,7 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-using WpfClient.CRUDAPI;
+using WpfClient.Service;
 
 namespace WpfClient
 {
@@ -18,40 +20,137 @@ namespace WpfClient
     /// </summary>
     public partial class Pessoas : Window
     {
-        private ICRUDAPIClient Api;
-        public int SelectedPessoaId { get; set; }
+        private PessoasClient Client;
+        public int? SelectedPessoaId { get; set; }
 
 
-        public Pessoas(ICRUDAPIClient api)
+        public Pessoas()
         {
-            Api = api;
+            Client = new PessoasClient(new System.Net.Http.HttpClient());
             InitializeComponent();
+            LoadDataGridPessoas();
         }
 
         public async void LoadDataGridPessoas()
         {
-            var pessoas = await Api.Pessoas.GetPessoasAsync();
-            dataGridPessoas.ItemsSource = pessoas;
-        }
+            try
+            {
+                var pessoas = await Client.GetPessoasAsync();
+                dataGridPessoas.ItemsSource = pessoas.ToList();
+            }
+            catch(ApiException ex)
+            {
 
-        public async void LoadPessoa()
-        {
-
+            }
         }
 
         public async void AddPessoa()
         {
+            try
+            {
+                var pessoa = new Pessoa
+                {
+                    Nome = nome.Text,
+                    Sobrenome = sobrenome.Text,
+                    Telefone = telefone.Text,
+                };
+                await Client.PostPessoaAsync(pessoa);
+            }
+            catch (ApiException ex)
+            {
 
+            }
+            ResetDados();
+
+            LoadDataGridPessoas();
         }
 
         public async void UpdatePessoa()
         {
+            try
+            {
+                int id = SelectedPessoaId.Value;
+                var pessoa = new Pessoa
+                {
+                    Id = id,
+                    Nome = nome.Text,
+                    Sobrenome = sobrenome.Text,
+                    Telefone = telefone.Text,
+                };
+                await Client.PutPessoaAsync(id, pessoa);
+            }
+            catch (ApiException ex)
+            {
 
+            }
+            ResetDados();
+
+            LoadDataGridPessoas();
         }
 
         public async void DeletePessoa()
         {
+            try
+            {
+                await Client.DeletePessoaAsync(SelectedPessoaId.Value);
+            }
+            catch (ApiException ex)
+            {
 
+            }
+            ResetDados();
+
+            LoadDataGridPessoas();
+        }
+
+        private void BtnGravar_Click(object sender, RoutedEventArgs e)
+        {
+            if(SelectedPessoaId == null)
+            {
+                AddPessoa();
+            }
+            else
+            {
+                UpdatePessoa();
+            }
+        }
+
+        private void dataGridPessoas_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var selected = dataGridPessoas.SelectedItem;
+            if (selected != null)
+            {
+                var pessoa = selected as Pessoa;
+                SelectedPessoaId = pessoa.Id;
+                nome.Text = pessoa.Nome;
+                sobrenome.Text = pessoa.Sobrenome;
+                telefone.Text = pessoa.Telefone;
+                BtnGravarText.Text = "Atualizar";
+                BtnExcluir.IsEnabled = true;
+            }
+        }
+
+        private void BtnCancelar_Click(object sender, RoutedEventArgs e)
+        {
+            ResetDados();
+        }
+
+        private void ResetDados()
+        {
+            SelectedPessoaId = null;
+            nome.Clear();
+            sobrenome.Clear();
+            telefone.Clear();
+            BtnExcluir.IsEnabled = false;
+            BtnGravarText.Text = "Gravar";
+        }
+
+        private void BtnExcluir_Click(object sender, RoutedEventArgs e)
+        {
+            if(SelectedPessoaId != null)
+            {
+                DeletePessoa();
+            }
         }
     }
 }
